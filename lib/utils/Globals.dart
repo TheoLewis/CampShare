2a55f9181e387875d1b4fd0b6e921ae7bfa08c25
theo_lewis <theo@zutapp.com>
2022-04-23 19:02:26 +1000

    Update

--- lib/utils/Globals.dart
+++ lib/utils/Globals.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 bool? isGuest;
@@ -12,4 +13,49 @@ double screenHeight(context) {
 
 void swapPagesNoContext(context, view) {
   Navigator.push(context,MaterialPageRoute(builder: (context) => view),);
+}
+
+Future<dynamic> showCustomDialog(BuildContext context,
+    { required String title,
+      required String bodyText,
+      required GestureTapCallback? onTap,
+    }) async {
+  showCupertinoDialog(
+      context: context,
+      builder: (_) {
+        return CupertinoAlertDialog(
+          title: Text(title),
+          content: Text(bodyText),
+          actions: <Widget>[
+            CupertinoDialogAction(
+                child: const Text("Cancel"),
+                onPressed: () => Navigator.pop(context)),
+            CupertinoDialogAction(
+                child: const Text("Ok"),
+                onPressed: onTap)
+          ],
+        );
+      }
+  );
+}
+
+Future<dynamic> showCustomDialogNoCancel(BuildContext context,
+    { required String title,
+      required String bodyText,
+      required GestureTapCallback? onTap,
+    }) async {
+  showCupertinoDialog(
+      context: context,
+      builder: (_) {
+        return CupertinoAlertDialog(
+          title: Text(title),
+          content: Text(bodyText),
+          actions: <Widget>[
+            CupertinoDialogAction(
+                child: const Text("OK"),
+                onPressed: onTap)
+          ],
+        );
+      }
+  );
 }
--- lib/views/PlatformMapView.dart
+++ lib/views/PlatformMapView.dart
@@ -1,6 +1,9 @@
+import 'package:campshare/utils/Globals.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_vibrate/flutter_vibrate.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:platform_maps_flutter/platform_maps_flutter.dart';
-import 'package:vibration/vibration.dart';
 
 class PlatformMapView extends StatefulWidget {
   bool? isGuest;
@@ -12,26 +15,50 @@ class PlatformMapView extends StatefulWidget {
 
 class _PlatformMapViewState extends State<PlatformMapView> {
 
+  Set<Marker> markers = {};
+
   @override
   void initState() {
     super.initState();
   }
 
-  void createMarker(tapPos) {
-    Vibration.vibrate(duration: 500);
+  void mapLongPress(LatLng tapPos) {
+    Vibrate.feedback(FeedbackType.selection);
+    showCustomDialog(context, title: "Save location?", bodyText: "Would you like to save this location?", onTap: () {createMarker(tapPos); Navigator.pop(context);});
+  }
+
+  void createMarker(LatLng tapPos) {
+    Marker marker = Marker(
+      markerId: MarkerId("Location $tapPos"),
+      position: LatLng(tapPos.latitude, tapPos.longitude),
+      icon: BitmapDescriptor.defaultMarker,
+      onTap: () {
+        Fluttertoast.showToast(
+            msg: "This is Center Short Toast",
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.CENTER,
+            timeInSecForIosWeb: 1,
+            backgroundColor: Colors.red,
+            textColor: Colors.white,
+            fontSize: 16.0
+        );
+      }
+    );
+    setState(() {
+      markers.add(marker);
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: PlatformMap(
-            initialCameraPosition: CameraPosition(target: LatLng(-37.971237,144.4926947), zoom: 5),
-            onLongPress: (tapPos) {},
-          ),
+      body: Center(
+        child: PlatformMap(
+          initialCameraPosition: CameraPosition(target: LatLng(-37.971237,144.4926947), zoom: 5),
+          onLongPress: (tapPos) {mapLongPress(tapPos);},
+          markers: markers,
         ),
-      )
+      ),
     );
   }
 }
